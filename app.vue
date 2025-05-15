f31bc1e34e988474be912635c655040bf1648358
VictorMRD <victormrd2002@hotmail.com>
2025-05-15 15:24:27 -0700

    Changes on navmenu

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import type { NavigationMenuItem } from '@nuxt/ui';
+    import { useMediaQuery } from '@vueuse/core'
 
     const items = [
     {
@@ -36,15 +37,18 @@
         colorMode.preference = _isDark ? 'dark' : 'light'
     }
     })
+
+    const checkSmallScreen = useMediaQuery('(max-width: 639px)')
+    const collap = computed(() => (checkSmallScreen.value ? true : false))
 </script>
 
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
-    <div class="flex justify-between items-center px-10 border-b-1 border-gray-300 shadow-xs fixed w-full light:bg-white dark:bg-slate-900 z-10">
-      <UButton class="w-fit text-nowrap font-semibold select-none bg-white dark:text-white dark:hover:bg-slate-900 dark:bg-slate-900 text-black font-mono hover:bg-white" to="#inicio">K-Shine</UButton>
-      <UNavigationMenu :items="items" class="w-full justify-center items-center font-mono"/>
-      <ClientOnly v-if="!colorMode?.forced">
+    <div class="flex max-sm:flex-col justify-between items-center px-10 border-b-1 border-gray-300 shadow-xs fixed w-full light:bg-white dark:bg-slate-900 z-10">
+      <UButton class="w-fit text-nowrap max-sm:text-2xl font-semibold select-none bg-white dark:text-white dark:hover:bg-slate-900 dark:bg-slate-900 text-black font-mono hover:bg-white" to="#inicio">K-Shine</UButton>
+      <UNavigationMenu :items="items" class="lg:w-full lg:justify-center lg:items-center lg:font-mono max-sm:flex-col"/>
+      <ClientOnly v-if="!colorMode?.forced && collap != true">
         <UButton
         :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
         color="neutral"
